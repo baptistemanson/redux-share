1205e2841774a036b88fefffa6ea74d3a37b36c9
Lawrence Vo <lawrencetvo@gmail.com>
2017-05-06 16:32:59 -0700

    added basic test cases

--- tests/basic-test.js
+++ tests/basic-test.js
@@ -1,4 +1,29 @@
-var express = require('express');
-var app = express();
+var chai = require('chai');
+var expect = chai.expect;
+chai.use(require('chai-as-promised'));
 
+var reducers = require('./reducers');
+
+describe('basic server and client', function() {
+
+  it('should run without any errors thrown', function() {
+    var server = require('./server-store')(reducers);
+    var client = require('./client-store')(reducers);
+  });
+
+});
+
+describe('communication between server and client', function() {
+
+  it('should recieve actions from the client', function(done) {
+    var server = require('./server-store')(reducers);
+    var client = require('./client-store')(reducers);
+    client.dispatch({ type: 'INCREMENT' });
+    setTimeout(function() {
+      expect(server.getState().likes).to.equal(1);
+      done();
+    }, 50);
+  });
+
+});
 
---
+++ tests/reducers.js
@@ -0,0 +1,13 @@
+var redux = require('redux');
+function likes(state = 0, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+module.exports = redux.combineReducers({ likes: likes });
